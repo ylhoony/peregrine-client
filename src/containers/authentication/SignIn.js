2dc9bef669bd4b3a4805cd67381a2ec65f96612d
Hoon Lee <yl.hoony@gmail.com>
2018-10-26 16:45:41 -0700

    created SignIn and SignUp container file, and added base.

---
+++ src/containers/authentication/SignIn.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from "react";
+
+class SignIn extends Component {
+  render() {
+    return (
+      <Fragment>
+        <div>SignIn page</div>
+      </Fragment>
+    );
+  }
+}
+
+export default SignIn;
---
+++ src/containers/authentication/SignUp.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from "react";
+
+class SignUp extends Component {
+  render() {
+    return (
+      <Fragment>
+        <div>SignUp page</div>
+      </Fragment>
+    );
+  }
+}
+
+export default SignUp;
