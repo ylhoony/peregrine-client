dfb6aacc23f99b519e386bf959ce3f84da676aec
Hoon Lee <yl.hoony@gmail.com>
2018-10-29 18:35:28 -0700

    imported material-ui for theme setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core";
+import { muiTheme } from "./services/muiTheme";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+const theme = createMuiTheme(muiTheme);
+
+ReactDOM.render(
+  // <Provider>
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline>
+        <App />
+      </CssBaseline>
+    </MuiThemeProvider>,
+  // </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
