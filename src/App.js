dd2f2020df6a6a3abcaf5ed2ef6cf1194c2f63a4
Hoon Lee <yl.hoony@gmail.com>
2018-10-29 18:46:31 -0700

    imported sign-in, sign-up page and set up react-router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import SignUp from "./containers/authentication/SignUp";
+import SignIn from "./containers/authentication/SignIn";
 
 import Demand from "./containers/demand/Demand";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Demand />
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/signin" component={SignIn} />
+
+          <Route exact path="/demand" component={Demand} />
+        </Switch>
+      </Router>
     );
   }
 }
