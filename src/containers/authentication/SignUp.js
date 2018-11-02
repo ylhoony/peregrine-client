9ddaf3a0395ed6fe3ca715314dec4ca0e2762cfb
Hoon Lee <yl.hoony@gmail.com>
2018-11-01 20:05:42 -0700

    added template form

--- src/containers/authentication/SignUp.js
+++ src/containers/authentication/SignUp.js
@@ -1,9 +1,54 @@
 import React, { Component, Fragment } from "react";
 import axios from "axios";
 
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import FormControl from "@material-ui/core/FormControl";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import LockIcon from "@material-ui/icons/LockOutlined";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import withStyles from "@material-ui/core/styles/withStyles";
+
+const styles = theme => ({
+  layout: {
+    width: "auto",
+    display: "block", // Fix IE 11 issue.
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+      width: 400,
+      marginLeft: "auto",
+      marginRight: "auto"
+    }
+  },
+  paper: {
+    marginTop: theme.spacing.unit * 8,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme
+      .spacing.unit * 3}px`
+  },
+  avatar: {
+    margin: theme.spacing.unit,
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing.unit
+  },
+  submit: {
+    marginTop: theme.spacing.unit * 3
+  }
+});
+
 class SignUp extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       user: {
@@ -30,48 +75,89 @@ class SignUp extends Component {
 
   handleFormSubmit = async e => {
     e.preventDefault();
-    console.log("formsubmit state: ", this.state);
+    console.log("form submit state: ", this.state);
 
     axios({
       method: "post",
       url: "api/v1/sign_up",
       data: this.state
-    }).then(res => {
-      console.log(res);
-    });
+    })
+      .then(res => {
+        console.log(res);
+        // debugger;
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
 
   render() {
+    const { classes } = this.props;
+
     return (
       <Fragment>
-        <div>SignUp page</div>
-        <form onSubmit={e => this.handleFormSubmit(e)}>
-          <label>email: </label>
-          <input
-            type="text"
-            name="email"
-            onChange={e => this.handleInputChange(e)}
-          />
-          <br />
-          <label>password: </label>
-          <input
-            type="password"
-            name="password"
-            onChange={e => this.handleInputChange(e)}
-          />
-          <br />
-          <label>password confirmation: </label>
-          <input
-            type="password"
-            name="password_confirmation"
-            onChange={e => this.handleInputChange(e)}
-          />
-          <br />
-          <button type="submit">sign up</button>
-        </form>
+        <main className={classes.layout}>
+          <Paper className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <LockIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign up
+            </Typography>
+            <form
+              className={classes.form}
+              onSubmit={e => this.handleFormSubmit(e)}
+            >
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="email">Email Address</InputLabel>
+                <Input
+                  id="email"
+                  name="email"
+                  autoFocus
+                  onChange={e => this.handleInputChange(e)}
+                />
+              </FormControl>
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="password">Password</InputLabel>
+                <Input
+                  name="password"
+                  type="password"
+                  id="password"
+                  onChange={e => this.handleInputChange(e)}
+                  // autoComplete="current-password"
+                />
+              </FormControl>
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="password_confirmation">
+                  Password Confirmation
+                </InputLabel>
+                <Input
+                  name="password_confirmation"
+                  type="password"
+                  id="password_confirmation"
+                  onChange={e => this.handleInputChange(e)}
+                  // autoComplete="current-password"
+                />
+              </FormControl>
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                Sign up
+              </Button>
+            </form>
+          </Paper>
+        </main>
       </Fragment>
     );
   }
 }
 
-export default SignUp;
+export default withStyles(styles)(SignUp);
