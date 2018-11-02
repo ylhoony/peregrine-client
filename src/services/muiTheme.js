808d4c6dea0769d6ced5eb2bd708b7ea1cd28a63
Hoon Lee <yl.hoony@gmail.com>
2018-11-01 20:06:36 -0700

    created material-ui them file and set color

--- src/services/muiTheme.js
+++ src/services/muiTheme.js
@@ -1,4 +1,16 @@
+import teal from "@material-ui/core/colors/teal";
+import blue from "@material-ui/core/colors/blue";
+
 export const muiTheme = {
+  palette: {
+    background: {
+      default: "#fff"
+    },
+    primary: {
+      main: teal[400]
+    },
+    secondary: blue
+  },
   typography: {
     useNextVariants: true
   }
