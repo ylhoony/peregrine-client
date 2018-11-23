9034c9f32778fc951a5dc728924f251328d7a46e
Hoon Lee <yl.hoony@gmail.com>
2018-11-23 15:27:35 -0800

    imported and set  countriesReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import authenticationReducer from "./authenticationReducer";
+import countriesReducer from "./countriesReducer";
 
 export const rootReducer = combineReducers({
-  authentication: authenticationReducer
+  authentication: authenticationReducer,
+  countries: countriesReducer
 });
