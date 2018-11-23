f6197d4f7b9479468257ea5f52280fcb3e715295
Hoon Lee <yl.hoony@gmail.com>
2018-11-23 15:27:56 -0800

    set up testing layouts

--- src/containers/setting/Setting.js
+++ src/containers/setting/Setting.js
@@ -1,48 +1,128 @@
 import React, { Component, Fragment } from "react";
+import { Link } from "react-router-dom";
+
+// implemnt card for config sections
+import { withStyles } from "@material-ui/core";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
+
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorderOutlined";
+
+const styles = theme => ({
+  paper: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit
+  },
+  section: {
+    display: "flex",
+    justifyContent: "flex-start"
+  },
+  article: {
+    display: "flex",
+    justifyContent: "space-between",
+    paddingTop: theme.spacing.unit * 2
+  },
+  card: {
+    width: "30%",
+    height: theme.spacing.unit * 12
+  },
+  cardHeader: {
+    textDecoration: "none"
+  }
+});
+
+// Show the list of settings required.
 
 class Setting extends Component {
   render() {
+    const { classes } = this.props;
+
     return (
       <Fragment>
         <main className={this.props.mainClassName}>
           <div className={this.props.toolbarClassName} />
-          <h2>Setting Page</h2>
+          <Paper className={classes.paper}>
+            <Typography variant="h6">Setting</Typography>
 
-          <Typography paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-            dolor purus non enim praesent elementum facilisis leo vel. Risus at
-            ultrices mi tempus imperdiet. Semper risus in hendrerit gravida
-            rutrum quisque non tellus. Convallis convallis tellus id interdum
-            velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean
-            sed adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-            integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-            eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-            quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-            vivamus at augue. At augue eget arcu dictum varius duis at
-            consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-            donec massa sapien faucibus et molestie ac.
-          </Typography>
-          <Typography paragraph>
-            Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-            ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-            elementum integer enim neque volutpat ac tincidunt. Ornare
-            suspendisse sed nisi lacus sed viverra tellus. Purus sit amet
-            volutpat consequat mauris. Elementum eu facilisis sed odio morbi.
-            Euismod lacinia at quis risus sed vulputate odio. Morbi tincidunt
-            ornare massa eget egestas purus viverra accumsan in. In hendrerit
-            gravida rutrum quisque non tellus orci ac. Pellentesque nec nam
-            aliquam sem et tortor. Habitant morbi tristique senectus et.
-            Adipiscing elit duis tristique sollicitudin nibh sit. Ornare aenean
-            euismod elementum nisi quis eleifend. Commodo viverra maecenas
-            accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam
-            ultrices sagittis orci a.
-          </Typography>
+            <section>
+              <article className={classes.article}>
+                <Card classes={{ root: classes.card }}>
+                  <CardHeader
+                    avatar={
+                      <FavoriteBorderIcon className={classes.cardHeader} />
+                    }
+                    className={classes.cardHeader}
+                    title="Account Addresses"
+                    titleTypographyProps={{ variant: "body1" }}
+                    subheader="Manage your compnay addresses"
+                    subheaderTypographyProps={{ variant: "body2" }}
+                    component={Link}
+                    to="/account-addresses"
+                  />
+                </Card>
+
+                <Card classes={{ root: classes.card }}>
+                  <CardHeader
+                    avatar={<FavoriteBorderIcon />}
+                    title="Account Contacts"
+                    titleTypographyProps={{ variant: "body1" }}
+                    subheader="Manage your compnay addresses"
+                    subheaderTypographyProps={{ variant: "body2" }}
+                  />
+                </Card>
+
+                <Card classes={{ root: classes.card }}>
+                  <CardHeader
+                    avatar={<FavoriteBorderIcon />}
+                    title="Payment Terms"
+                    titleTypographyProps={{ variant: "body1" }}
+                    subheader="Manage your compnay addresses"
+                    subheaderTypographyProps={{ variant: "body2" }}
+                  />
+                </Card>
+              </article>
+
+              <article className={classes.article}>
+                <Card classes={{ root: classes.card }}>
+                  <CardHeader
+                    avatar={<FavoriteBorderIcon />}
+                    title="Account Addresses"
+                    titleTypographyProps={{ variant: "body1" }}
+                    subheader="Manage your compnay addresses"
+                    subheaderTypographyProps={{ variant: "body2" }}
+                  />
+                </Card>
+
+                <Card classes={{ root: classes.card }}>
+                  <CardHeader
+                    avatar={<FavoriteBorderIcon />}
+                    title="Account Contacts"
+                    titleTypographyProps={{ variant: "body1" }}
+                    subheader="Manage your compnay addresses"
+                    subheaderTypographyProps={{ variant: "body2" }}
+                  />
+                </Card>
+
+                <Card classes={{ root: classes.card }}>
+                  <CardHeader
+                    avatar={<FavoriteBorderIcon />}
+                    title="Account Contacts"
+                    titleTypographyProps={{ variant: "body1" }}
+                    subheader="Manage your compnay addresses"
+                    subheaderTypographyProps={{ variant: "body2" }}
+                  />
+                </Card>
+              </article>
+            </section>
+          </Paper>
         </main>
       </Fragment>
     );
   }
 }
 
-export default Setting;
+export default withStyles(styles)(Setting);
