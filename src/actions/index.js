f5b34d13c39dd3f0214b9c029939e615c362e68a
Hoon Lee <yl.hoony@gmail.com>
2018-11-23 15:25:14 -0800

    imported and set countriesActions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,12 @@
 import authentication from "./authenticationActions";
+import countries from "./countriesActions";
 
 export const actions = {
+  // authentication
   signUp: authentication.signUp,
   signIn: authentication.signIn,
-  authenticateUser: authentication.authenticateUser
+  authenticateUser: authentication.authenticateUser,
+  // countries
+  fetchCountries: countries.fetchCountries,
+  fetchCountry: countries.fetchCountry
 };
