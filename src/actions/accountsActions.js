b7d1654f5868e1b9f652b9e10562ada4762dc170
Hoon Lee <yl.hoony@gmail.com>
2018-11-23 15:23:45 -0800

    created file and set up basic structure

---
+++ src/actions/accountsActions.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+import authentication from "../services/authentication";
+
+// actions for fetching account list
+export const FETCH_ACCOUNTS_START = "FETCH_ACCOUNTS_START";
+export const FETCH_ACCOUNTS_SUCCESS = "FETCH_ACCOUNTS_SUCCESS";
+export const FETCH_ACCOUNTS_FAILURE = "FETCH_ACCOUNTS_FAILURE";
+export const FETCH_ACCOUNTS_ERROR = "FETCH_ACCOUNTS_ERROR";
+
+// actions for creating new account
+export const CREATE_ACCOUNT_START = "CREATE_ACCOUNT_START";
+export const CREATE_ACCOUNT_SUCCESS = "CREATE_ACCOUNT_SUCCESS";
+export const CREATE_ACCOUNT_FAILURE = "CREATE_ACCOUNT_FAILURE";
+export const CREATE_ACCOUNT_ERROR = "CREATE_ACCOUNT_ERROR";
+
+// actions for fetching one account
+export const FETCH_ACCOUNT_START = "FETCH_ACCOUNT_START";
+export const FETCH_ACCOUNT_SUCCESS = "FETCH_ACCOUNT_SUCCESS";
+export const FETCH_ACCOUNT_FAILURE = "FETCH_ACCOUNT_FAILURE";
+export const FETCH_ACCOUNT_ERROR = "FETCH_ACCOUNT_ERROR";
+
+// actions for updating account
+export const UPDATE_ACCOUNT_START = "UPDATE_ACCOUNT_START";
+export const UPDATE_ACCOUNT_SUCCESS = "UPDATE_ACCOUNT_SUCCESS";
+export const UPDATE_ACCOUNT_FAILURE = "UPDATE_ACCOUNT_FAILURE";
+export const UPDATE_ACCOUNT_ERROR = "UPDATE_ACCOUNT_ERROR";
+
+export default {
+  fetchAccounts: () => {
+    return dispatch => {};
+  },
+  createAccount: data => {
+    return dispatch => {};
+  },
+  fetchAccount: data => {}
+};
