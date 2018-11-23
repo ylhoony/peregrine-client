8034ead628152ec2d84b14564940761358b499a5
Hoon Lee <yl.hoony@gmail.com>
2018-11-22 17:08:21 -0800

    updated auth actions

--- src/actions/authenticationActions.js
+++ src/actions/authenticationActions.js
@@ -2,19 +2,24 @@ import axios from "axios";
 import authentication from "../services/authentication";
 
 export const SIGN_UP_USER_START = "SIGN_UP_USER_START";
-export const SIGN_UP_USER_SUCCESS = "SIGN_UP_USER_START_SUCCESS";
-export const SIGN_UP_USER_FAILURE = "SIGN_UP_USER_START_FAILURE";
-export const SIGN_UP_USER_ERROR = "SIGN_UP_USER_START_ERROR";
+export const SIGN_UP_USER_SUCCESS = "SIGN_UP_USER_SUCCESS";
+export const SIGN_UP_USER_FAILURE = "SIGN_UP_USER_FAILURE";
+export const SIGN_UP_USER_ERROR = "SIGN_UP_USER_ERROR";
 
 export const SIGN_IN_USER_START = "SIGN_IN_USER_START";
 export const SIGN_IN_USER_SUCCESS = "SIGN_IN_USER_SUCCESS";
+export const SIGN_IN_USER_FAILURE = "SIGN_IN_USER_FAILURE";
 export const SIGN_IN_USER_ERROR = "SIGN_IN_USER_ERROR";
 
 export const SIGN_OUT_USER_START = "SIGN_OUT_USER_START";
 export const SIGN_OUT_USER_SUCCESS = "SIGN_OUT_USER_SUCCESS";
+export const SIGN_OUT_USER_FAILURE = "SIGN_OUT_USER_FAILURE";
 export const SIGN_OUT_USER_END = "SIGN_OUT_USER_END";
 
-export const AUTHENTICATE_USER = "AUTHENTICATE_USER";
+export const AUTHENTICATE_USER_START = "AUTHENTICATE_USER_START";
+export const AUTHENTICATE_USER_SUCCESS = "AUTHENTICATE_USER_SUCCESS";
+export const AUTHENTICATE_USER_FAILURE = "AUTHENTICATE_USER_FAILURE";
+export const AUTHENTICATE_USER_ERROR = "AUTHENTICATE_USER_ERROR";
 
 export default {
   signUp: data => {
@@ -25,19 +30,17 @@ export default {
 
         if (!!res.headers.authorization) {
           authentication.setToken(res.headers.authorization);
-          console.log(res);
           dispatch({
             type: SIGN_UP_USER_SUCCESS,
             payload: res.data
           });
         } else {
           dispatch({
-            type: SIGN_UP_USER_FAILURE,
+            type: SIGN_UP_USER_FAILURE
             // payload: res
           });
         }
       } catch (err) {
-        console.log(err);
         dispatch({ type: SIGN_UP_USER_ERROR, payload: err });
       }
     };
@@ -46,27 +49,36 @@ export default {
     return async dispatch => {
       dispatch({ type: SIGN_IN_USER_START });
       try {
-        const res = await axios.post("/api/v1/sign_up", data);
-        // console.log(res);
-        dispatch({
-          type: SIGN_IN_USER_SUCCESS,
-          payload: res.headers.authorization
-        });
+        const res = await axios.post("/api/v1/sign_in", data);
+        if (!!res.headers.authorization) {
+          authentication.setToken(res.headers.authorization);
+          dispatch({
+            type: SIGN_IN_USER_SUCCESS,
+            payload: res.data
+          });
+        } else {
+          dispatch({
+            type: SIGN_IN_USER_FAILURE
+          });
+        }
       } catch (err) {
-        console.log(err);
-        dispatch({ type: SIGN_IN_USER_SUCCESS, payload: err });
+        dispatch({ type: SIGN_IN_USER_ERROR, payload: err });
       }
     };
   },
   authenticateUser: () => {
     return async dispatch => {
-      console.log(authentication.token());
-      const res = axios.get("/api/v1/authenticate", {
-        headers: {
-          Authorization: authentication.token()
-        }
-      });
-      dispatch({ type: AUTHENTICATE_USER, payload: res });
+      dispatch({ type: AUTHENTICATE_USER_START });
+      try {
+        const res = await axios.get("/api/v1/authenticate", {
+          headers: {
+            Authorization: authentication.getEncodedToken()
+          }
+        });
+        dispatch({ type: AUTHENTICATE_USER_SUCCESS, payload: res.data });
+      } catch (err) {
+        dispatch({ type: AUTHENTICATE_USER_ERROR, payload: err });
+      }
     };
   }
 };
