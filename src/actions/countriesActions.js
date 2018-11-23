76383efff5afe7429440501e72aa0a51cfc691e6
Hoon Lee <yl.hoony@gmail.com>
2018-11-23 15:22:26 -0800

    setup actions for fetching country data

---
+++ src/actions/countriesActions.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+import authentication from "../services/authentication";
+
+// actions for fetching account list
+export const FETCH_COUNTRIES_START = "FETCH_COUNTRIES_START";
+export const FETCH_COUNTRIES_SUCCESS = "FETCH_COUNTRIES_SUCCESS";
+export const FETCH_COUNTRIES_FAILURE = "FETCH_COUNTRIES_FAILURE";
+export const FETCH_COUNTRIES_ERROR = "FETCH_COUNTRIES_ERROR";
+
+// actions for fetching one account
+export const FETCH_COUNTRY_START = "FETCH_COUNTRY_START";
+export const FETCH_COUNTRY_SUCCESS = "FETCH_COUNTRY_SUCCESS";
+export const FETCH_COUNTRY_FAILURE = "FETCH_COUNTRY_FAILURE";
+export const FETCH_COUNTRY_ERROR = "FETCH_COUNTRY_ERROR";
+
+export default {
+  fetchCountries: () => {
+    return async dispatch => {
+      dispatch({ type: FETCH_COUNTRIES_START });
+      try {
+        const res = await axios.get("/api/v1/countries", {
+          headers: {
+            Authorization: authentication.getEncodedToken()
+          }
+        });
+        console.log(res.data);
+        dispatch({ type: FETCH_COUNTRIES_SUCCESS, payload: res.data });
+      } catch (err) {
+        console.log(err);
+        dispatch({ type: FETCH_COUNTRIES_ERROR });
+      }
+    };
+  },
+  fetchCountry: countryId => {
+    return async dispatch => {
+      dispatch({ type: FETCH_COUNTRY_START });
+      try {
+        const res = await axios.get(`/api/v1/countries/${countryId}`, {
+          headers: {
+            Authorization: authentication.getEncodedToken()
+          }
+        });
+        console.log(res.data);
+        dispatch({ type: FETCH_COUNTRY_SUCCESS, payload: res.data });
+      } catch (err) {
+        console.log(err);
+        dispatch({ type: FETCH_COUNTRY_ERROR });
+      }
+    };
+  }
+};
