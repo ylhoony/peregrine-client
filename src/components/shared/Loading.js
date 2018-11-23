396d78439696a32d8ec2096c5cafcfc1b7a86969
Hoon Lee <yl.hoony@gmail.com>
2018-11-22 17:09:33 -0800

    created/set basic loading file

---
+++ src/components/shared/Loading.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from "react";
+
+class Loading extends Component {
+  render() {
+    return (
+      <Fragment>
+        <p>Loading...</p>
+      </Fragment>
+    );
+  }
+}
+
+export default Loading;
