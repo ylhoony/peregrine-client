6c889cd9f99753e4ac85ce72de5836c50180b424
Hoon Lee <yl.hoony@gmail.com>
2018-11-26 20:10:23 -0800

    updated list menu

--- src/containers/layout/LeftDrawer.js
+++ src/containers/layout/LeftDrawer.js
@@ -2,11 +2,10 @@ import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { Link } from "react-router-dom";
+import { withStyles } from "@material-ui/core/styles";
 import { actions } from "../../actions/index";
 
 // mui
-import { withStyles } from "@material-ui/core/styles";
-
 import Drawer from "@material-ui/core/Drawer";
 import Divider from "@material-ui/core/Divider";
 import List from "@material-ui/core/List";
@@ -14,7 +13,11 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import Toolbar from "@material-ui/core/Toolbar";
+import Collapse from "@material-ui/core/Collapse";
 
+// Icons
+import ExpandLess from "@material-ui/icons/ExpandLess";
+import ExpandMore from "@material-ui/icons/ExpandMore";
 import DashboardIcon from "@material-ui/icons/DashboardOutlined";
 import StoreIcon from "@material-ui/icons/StoreOutlined";
 import CartIcon from "@material-ui/icons/ShoppingCartOutlined";
@@ -27,6 +30,10 @@ import ChevronRightIcon from "@material-ui/icons/ChevronRightOutlined";
 import ShareIcon from "@material-ui/icons/ShareOutlined";
 import SettingsIcon from "@material-ui/icons/SettingsOutlined";
 import IconButton from "@material-ui/core/IconButton";
+import RemoveIcon from "@material-ui/icons/RemoveOutlined";
+// import RadioButton from "@material-ui/icons/RadioButtonUncheckedOutlined";
+// import RadioButton from "@material-ui/icons/RadioButtonCheckedOutlined";
+import SubDirIcon from "@material-ui/icons/SubdirectoryArrowRightOutlined";
 
 const drawerWidth = 200;
 
@@ -43,17 +50,57 @@ const styles = theme => ({
     alignItems: "center",
     padding: "2 8px",
     justifyContent: "flex-end"
+  },
+  listItem: {
+    paddingLeft: theme.spacing.unit * 2,
+    paddingRight: theme.spacing.unit * 2
+  },
+  listItemIcon: {
+    marginRight: theme.spacing.unit
+  },
+  listItemText: {
+    paddingLeft: theme.spacing.unit
+  },
+  nested: {
+    paddingLeft: theme.spacing.unit * 3
   }
 });
 
 class LeftDrawer extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      productListOpen: false,
+      demandListOpen: false,
+      supplyListOpen: false,
+      warehouseListOpen: false,
+      logisticsListOpen: false
+    };
+  }
+
   handleLeftDrawerClose = () => {
     this.props.actions.closeLeftDrawer();
   };
 
+  handleListOpen = e => {
+    e.preventDefault();
+    console.log(e.target);
+    // debugger;
+    const listName =
+      e.target.parentNode.getAttribute("id") ||
+      e.target.parentNode.parentNode.getAttribute("id");
+    const key = `${listName}ListOpen`;
+    console.log(key);
+
+    this.setState({
+      [key]: !!this.state[key] ? false : true
+    });
+    console.log(this.state);
+  };
+
   render() {
     const { classes, leftDrawerOpen, theme } = this.props;
-
     console.log("left drawer props: ", this.props);
     return (
       <Fragment>
@@ -81,69 +128,388 @@ class LeftDrawer extends Component {
           </Toolbar>
 
           <Divider />
+
           <List>
-            <ListItem button dense key="Dashboard" component={Link} to="/">
-              <ListItemIcon>
+            {/* Dashboard */}
+            <ListItem
+              button
+              dense
+              key="Dashboard"
+              component={Link}
+              to="/"
+              className={classes.listItem}
+            >
+              <ListItemIcon className={classes.listItemIcon}>
                 <DashboardIcon fontSize="small" />
               </ListItemIcon>
-              <ListItemText primary="Dashboard" fontSize="small" />
+              <ListItemText
+                primary="Dashboard"
+                fontSize="small"
+                className={classes.listItemText}
+              />
             </ListItem>
-
-            <ListItem button dense key="Product" component={Link} to="/product">
-              <ListItemIcon>
+            {/* Product */}
+            <ListItem
+              button
+              component={Link}
+              className={classes.listItem}
+              dense
+              id="product"
+              key="Product"
+              to="/product"
+            >
+              <ListItemIcon className={classes.listItemIcon}>
                 <ViewListIcon fontSize="small" />
               </ListItemIcon>
-              <ListItemText primary="Product" />
+              <ListItemText
+                primary="Product"
+                className={classes.listItemText}
+              />
+              {this.state.productListOpen ? (
+                <ExpandLess
+                  fontSize="small"
+                  onClick={e => this.handleListOpen(e)}
+                />
+              ) : (
+                <ExpandMore
+                  fontSize="small"
+                  onClick={e => this.handleListOpen(e)}
+                />
+              )}
             </ListItem>
 
-            <ListItem button dense key="Demand" component={Link} to="/demand">
-              <ListItemIcon>
+            <Collapse
+              in={this.state.productListOpen}
+              timeout="auto"
+              unmountOnExit
+            >
+              <List component="div" disablePadding>
+                <ListItem button dense className={classes.nested}>
+                  <ListItemIcon className={classes.listItemIcon}>
+                    <SubDirIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText
+                    inset
+                    primary="Product Master"
+                    className={classes.listItemText}
+                  />
+                </ListItem>
+                <ListItem button dense className={classes.nested}>
+                  <ListItemIcon className={classes.listItemIcon}>
+                    <SubDirIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText
+                    inset
+                    primary="Product Family"
+                    className={classes.listItemText}
+                  />
+                </ListItem>
+                <ListItem button dense className={classes.nested}>
+                  <ListItemIcon className={classes.listItemIcon}>
+                    <SubDirIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText
+                    inset
+                    primary="Bill of Material"
+                    className={classes.listItemText}
+                  />
+                </ListItem>
+              </List>
+            </Collapse>
+
+            {/* Demand */}
+            <ListItem
+              button
+              component={Link}
+              className={classes.listItem}
+              dense
+              id="demand"
+              key="Demand"
+              to="/demand"
+            >
+              <ListItemIcon className={classes.listItemIcon}>
                 <StoreIcon fontSize="small" />
               </ListItemIcon>
-              <ListItemText primary="Demand" />
+              <ListItemText primary="Demand" className={classes.listItemText} />
+
+              {this.state.demandListOpen ? (
+                <ExpandLess
+                  fontSize="small"
+                  onClick={e => this.handleListOpen(e)}
+                />
+              ) : (
+                <ExpandMore
+                  fontSize="small"
+                  onClick={e => this.handleListOpen(e)}
+                />
+              )}
             </ListItem>
 
-            <ListItem button dense key="Supply" component={Link} to="/supply">
-              <ListItemIcon>
+            <Collapse
+              in={this.state.demandListOpen}
+              timeout="auto"
+              unmountOnExit
+            >
+              <List component="div" disablePadding>
+                <ListItem button dense className={classes.nested}>
+                  <ListItemIcon className={classes.listItemIcon}>
+                    <SubDirIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText
+                    inset
+                    primary="Customer"
+                    className={classes.listItemText}
+                  />
+                </ListItem>
+                <ListItem button dense className={classes.nested}>
+                  <ListItemIcon className={classes.listItemIcon}>
+                    <SubDirIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText
+                    inset
+                    primary="Sales Order"
+                    className={classes.listItemText}
+                  />
+                </ListItem>
+                <ListItem button dense className={classes.nested}>
+                  <ListItemIcon className={classes.listItemIcon}>
+                    <SubDirIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText
+                    inset
+                    primary="Sales Invoice"
+                    className={classes.listItemText}
+                  />
+                </ListItem>
+              </List>
+            </Collapse>
+
+            {/* Supply */}
+            <ListItem
+              button
+              component={Link}
+              className={classes.listItem}
+              dense
+              id="supply"
+              key="Supply"
+              to="/supply"
+            >
+              <ListItemIcon className={classes.listItemIcon}>
                 <CartIcon fontSize="small" />
               </ListItemIcon>
-              <ListItemText primary="Supply" />
+              <ListItemText primary="Supply" className={classes.listItemText} />
+
+              {this.state.supplyListOpen ? (
+                <ExpandLess
+                  fontSize="small"
+                  onClick={e => this.handleListOpen(e)}
+                />
+              ) : (
+                <ExpandMore
+                  fontSize="small"
+                  onClick={e => this.handleListOpen(e)}
+                />
+              )}
             </ListItem>
 
+            <Collapse
+              in={this.state.supplyListOpen}
+              timeout="auto"
+              unmountOnExit
+            >
+              <List component="div" disablePadding>
+                <ListItem button dense className={classes.nested}>
+                  <ListItemIcon className={classes.listItemIcon}>
+                    <SubDirIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText
+                    inset
+                    primary="Supplier"
+                    className={classes.listItemText}
+                  />
+                </ListItem>
+                <ListItem button dense className={classes.nested}>
+                  <ListItemIcon className={classes.listItemIcon}>
+                    <SubDirIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText
+                    inset
+                    primary="Purchase Order"
+                    className={classes.listItemText}
+                  />
+                </ListItem>
+                <ListItem button dense className={classes.nested}>
+                  <ListItemIcon className={classes.listItemIcon}>
+                    <SubDirIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText
+                    inset
+                    primary="Bill"
+                    className={classes.listItemText}
+                  />
+                </ListItem>
+              </List>
+            </Collapse>
+
+            {/* Warehouse */}
             <ListItem
               button
-              dense
-              key="Warehouse"
               component={Link}
+              className={classes.listItem}
+              dense
+              id="warehouse"
+              key="Warehouse"
               to="/warehouse"
             >
-              <ListItemIcon>
+              <ListItemIcon className={classes.listItemIcon}>
                 <StorageIcon fontSize="small" />
               </ListItemIcon>
-              <ListItemText primary="Warehouse" />
+              <ListItemText
+                primary="Warehouse"
+                className={classes.listItemText}
+              />
+
+              {this.state.warehouseListOpen ? (
+                <ExpandLess
+                  fontSize="small"
+                  onClick={e => this.handleListOpen(e)}
+                />
+              ) : (
+                <ExpandMore
+                  fontSize="small"
+                  onClick={e => this.handleListOpen(e)}
+                />
+              )}
             </ListItem>
 
+            <Collapse
+              in={this.state.warehouseListOpen}
+              timeout="auto"
+              unmountOnExit
+            >
+              <List component="div" disablePadding>
+                <ListItem button dense className={classes.nested}>
+                  <ListItemIcon className={classes.listItemIcon}>
+                    <SubDirIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText
+                    inset
+                    primary="Warehouse"
+                    className={classes.listItemText}
+                  />
+                </ListItem>
+                <ListItem button dense className={classes.nested}>
+                  <ListItemIcon className={classes.listItemIcon}>
+                    <SubDirIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText
+                    inset
+                    primary="Inbound"
+                    className={classes.listItemText}
+                  />
+                </ListItem>
+                <ListItem button dense className={classes.nested}>
+                  <ListItemIcon className={classes.listItemIcon}>
+                    <SubDirIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText
+                    inset
+                    primary="Outbound"
+                    className={classes.listItemText}
+                  />
+                </ListItem>
+              </List>
+            </Collapse>
+
+            {/* Logistics */}
+            <ListItem
+              button
+              component={Link}
+              className={classes.listItem}
+              dense
+              id="logistics"
+              key="Logistics"
+              to="/logistics"
+            >
+              <ListItemIcon className={classes.listItemIcon}>
+                <ShippingIcon fontSize="small" />
+              </ListItemIcon>
+              <ListItemText
+                primary="Logistics"
+                className={classes.listItemText}
+              />
+
+              {this.state.logisticsListOpen ? (
+                <ExpandLess
+                  fontSize="small"
+                  onClick={e => this.handleListOpen(e)}
+                />
+              ) : (
+                <ExpandMore
+                  fontSize="small"
+                  onClick={e => this.handleListOpen(e)}
+                />
+              )}
+            </ListItem>
+
+            <Collapse
+              in={this.state.logisticsListOpen}
+              timeout="auto"
+              unmountOnExit
+            >
+              <List component="div" disablePadding>
+                <ListItem button dense className={classes.nested}>
+                  <ListItemIcon className={classes.listItemIcon}>
+                    <SubDirIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText
+                    inset
+                    primary="Carrier"
+                    className={classes.listItemText}
+                  />
+                </ListItem>
+                <ListItem button dense className={classes.nested}>
+                  <ListItemIcon className={classes.listItemIcon}>
+                    <SubDirIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText
+                    inset
+                    primary="Inbound"
+                    className={classes.listItemText}
+                  />
+                </ListItem>
+                <ListItem button dense className={classes.nested}>
+                  <ListItemIcon className={classes.listItemIcon}>
+                    <SubDirIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText
+                    inset
+                    primary="Outbound"
+                    className={classes.listItemText}
+                  />
+                </ListItem>
+              </List>
+            </Collapse>
+
+            {/* Report */}
             <ListItem
               button
               dense
-              key="Logistics"
+              key="Report"
               component={Link}
-              to="/logistics"
+              to="/report"
+              className={classes.listItem}
             >
-              <ListItemIcon>
-                <ShippingIcon fontSize="small" />
-              </ListItemIcon>
-              <ListItemText primary="Logistics" />
-            </ListItem>
-
-            <ListItem button dense key="Report" component={Link} to="/report">
-              <ListItemIcon>
+              <ListItemIcon className={classes.listItemIcon}>
                 <BarChartIcon fontSize="small" />
               </ListItemIcon>
-              <ListItemText primary="Report" />
+              <ListItemText primary="Report" className={classes.listItemText} />
             </ListItem>
           </List>
+
           <Divider />
+
+          {/* Integration */}
           <List>
             <ListItem
               button
@@ -151,18 +517,32 @@ class LeftDrawer extends Component {
               key="Integration"
               component={Link}
               to="/integration"
+              className={classes.listItem}
             >
-              <ListItemIcon>
+              <ListItemIcon className={classes.listItemIcon}>
                 <ShareIcon fontSize="small" />
               </ListItemIcon>
-              <ListItemText primary="Integration" />
+              <ListItemText
+                primary="Integration"
+                className={classes.listItemText}
+              />
             </ListItem>
 
-            <ListItem button dense key="Setting" component={Link} to="/setting">
-              <ListItemIcon>
+            <ListItem
+              button
+              dense
+              key="Setting"
+              component={Link}
+              to="/setting"
+              className={classes.listItem}
+            >
+              <ListItemIcon className={classes.listItemIcon}>
                 <SettingsIcon fontSize="small" />
               </ListItemIcon>
-              <ListItemText primary="Setting" />
+              <ListItemText
+                primary="Setting"
+                className={classes.listItemText}
+              />
             </ListItem>
           </List>
         </Drawer>
