5218a2525424347819b838c4e9aeb5494a03b189
Hoon Lee <yl.hoony@gmail.com>
2018-11-27 17:44:27 -0800

    created file with dummy structure

---
+++ src/containers/setting/account-contacts/AccountContacts.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from "react";
+
+class AccountContacts extends Component {
+  render() {
+    return (
+      <Fragment>
+        <div>Account Contacts page</div>
+      </Fragment>
+    );
+  }
+}
+
+export default AccountContacts;
