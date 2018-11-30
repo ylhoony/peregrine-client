e08879ffc8d23b1e5b0b830d127bb4df471460e4
Hoon Lee <yl.hoony@gmail.com>
2018-11-29 16:57:58 -0800

    added accounts

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import authentication from "./authenticationReducer";
 import countries from "./countriesReducer";
 import currencies from "./currenciesReducer";
 import layouts from "./layoutsReducers";
+import accounts from "./accountsReducer";
 
 export const rootReducer = combineReducers({
   authentication,
   countries,
   currencies,
-  layouts
+  layouts,
+  accounts
 });
