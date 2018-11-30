370928742041d26ba37a975dd1a009d87eb8dd1a
Hoon Lee <yl.hoony@gmail.com>
2018-11-30 15:49:32 -0800

    refactored to show accounts on popper with links

--- src/containers/layout/Header.js
+++ src/containers/layout/Header.js
@@ -10,39 +10,30 @@ import Breadcrumb from "../../components/shared/Breadcrumb";
 import classNames from "classnames";
 import { withStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
-import Select from "@material-ui/core/Select";
-import MenuItem from "@material-ui/core/MenuItem";
-import { fade } from "@material-ui/core/styles/colorManipulator";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Popper from "@material-ui/core/Popper";
+import Fade from "@material-ui/core/Fade";
+import Paper from "@material-ui/core/Paper";
+import Divider from "@material-ui/core/Divider";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
 
 // Icons
+import HomeIcon from "@material-ui/icons/HomeOutlined";
 import MenuIcon from "@material-ui/icons/MenuOutlined";
 import SearchIcon from "@material-ui/icons/Search";
 import PersonIcon from "@material-ui/icons/PersonOutlineOutlined";
 import EjectIcon from "@material-ui/icons/EjectOutlined";
-
-const drawerWidth = 200;
+import UnfoldIcon from "@material-ui/icons/UnfoldMoreOutlined";
 
 const styles = theme => ({
   appBar: {
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    })
+    zIndex: theme.zIndex.drawer + 1
   },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    })
-  },
-  hide: {
-    display: "none"
-  },
-  //
   navLeft: {
     alignItems: "center",
     display: "flex",
@@ -63,31 +54,107 @@ const styles = theme => ({
       paddingLeft: "12px",
       paddingRight: "12px"
     }
+  },
+  headerList: {
+    paddingTop: 0,
+    paddingBottom: 0,
+    width: "-webkit-fill-available"
+  },
+  headerListItem: {
+    paddingLeft: theme.spacing.unit,
+    paddingRight: theme.spacing.unit
+  },
+  headerListItemText: {
+    paddingRight: theme.spacing.unit * 0.5
+  },
+  breadcrumbWrapper: {
+    paddingLeft: theme.spacing.unit * 2
+  },
+  popper: {
+    zIndex: theme.zIndex.drawer + 1
   }
 });
 
 class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      anchorEl: null,
+      popperOpen: false
+    };
+  }
+
   handleChange = e => {
     this.setState({ [e.target.name]: e.target.value });
-    console.log(this.state);
   };
 
   handleSignOut = () => {
-    console.log("sign out!");
     authentication.removeToken();
     this.props.history.push("/signin");
   };
 
   handleLeftDrawerOpen = () => {
-    this.props.actions.openLeftDrawer();
+    !!this.props.leftDrawerOpen
+      ? this.props.actions.closeLeftDrawer()
+      : this.props.actions.openLeftDrawer();
+  };
+
+  handleHeaderPopperOpen = e => {
+    this.setState({
+      anchorEl: e.currentTarget,
+      popperOpen: !this.state.popperOpen
+    });
   };
 
   render() {
-    const { classes, leftDrawerOpen } = this.props;
-    // console.log("header props", this.props);
+    const { accounts, classes, currentAccount, leftDrawerOpen } = this.props;
+
+    let accountList;
+
+    if (!accounts.length) {
+      accountList = "";
+    } else {
+      accountList = accounts.map(account => {
+        return (
+          <ListItem button key={account.id}>
+            <ListItemText>{account.company_name}</ListItemText>
+          </ListItem>
+        );
+      });
+    }
 
     return (
       <Fragment>
+        <Popper
+          className={classes.popper}
+          open={this.state.popperOpen}
+          anchorEl={this.state.anchorEl}
+          placement="bottom-start"
+          transition
+        >
+          {({ TransitionProps }) => (
+            <Fade {...TransitionProps} timeout={350}>
+              <ClickAwayListener
+                onClickAway={e => this.handleHeaderPopperOpen(e)}
+              >
+                <Paper>
+                  <List component="nav" dense>
+                    {accountList}
+                  </List>
+                  <Divider />
+                  <List component="nav" dense>
+                    <ListItem button component={Link} to="/accounts">
+                      <ListItemText>See all accounts</ListItemText>
+                    </ListItem>
+                    <ListItem button component={Link} to="/accounts/new">
+                      <ListItemText>Add new organization</ListItemText>
+                    </ListItem>
+                  </List>
+                </Paper>
+              </ClickAwayListener>
+            </Fade>
+          )}
+        </Popper>
         <AppBar
           color="default"
           position="fixed"
@@ -102,16 +169,53 @@ class Header extends Component {
             classes={{ dense: classes.toolbar }}
           >
             <div className={classes.navLeft}>
-              <IconButton
-                aria-label="Open drawer"
-                color="inherit"
-                fontSize="small"
-                onClick={() => this.handleLeftDrawerOpen()}
-                className={classNames(leftDrawerOpen && classes.hide)}
-              >
-                <MenuIcon />
-              </IconButton>
-              <Breadcrumb />
+              {!!(window.location.pathname === "/accounts/new") ? (
+                <IconButton
+                  aria-label="Control drawer"
+                  color="inherit"
+                  fontSize="small"
+                  component={Link}
+                  to="/"
+                >
+                  <HomeIcon fontSize="small" />
+                </IconButton>
+              ) : (
+                <Fragment>
+                  <IconButton
+                    aria-label="Control drawer"
+                    color="inherit"
+                    fontSize="small"
+                    onClick={() => this.handleLeftDrawerOpen()}
+                  >
+                    <MenuIcon fontSize="small" />
+                  </IconButton>
+
+                  <List className={classes.headerList}>
+                    <ListItem
+                      button
+                      dense
+                      className={classes.headerListItem}
+                      onClick={e => this.handleHeaderPopperOpen(e)}
+                    >
+                      <ListItemText
+                        disableTypography
+                        className={classes.headerListItemText}
+                      >
+                        <Typography variant="subtitle1">
+                          {!!currentAccount
+                            ? currentAccount.company_name
+                            : "Create Account"}
+                        </Typography>
+                      </ListItemText>
+                      <UnfoldIcon fontSize="small" />
+                    </ListItem>
+                  </List>
+
+                  <div className={classes.breadcrumbWrapper}>
+                    <Breadcrumb />
+                  </div>
+                </Fragment>
+              )}
             </div>
 
             <div className={classes.navRight}>
@@ -138,16 +242,21 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
-const mapStateToProps = ({ layouts }) => {
+const mapStateToProps = ({ accounts, layouts }) => {
   return {
+    currentAccount: accounts.currentAccount,
+    accounts: accounts.accounts,
+
     leftDrawerWidth: layouts.leftDrawerWidth,
     leftDrawerOpen: layouts.leftDrawerOpen
   };
 };
 
 export default withStyles(styles, { withTheme: true })(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(withRouter(Header))
+  withRouter(
+    connect(
+      mapStateToProps,
+      mapDispatchToProps
+    )(Header)
+  )
 );
