c0edd5654bb98ae06544a575fb703ff5c0949e43
Hoon Lee <yl.hoony@gmail.com>
2018-12-05 18:27:49 -0800

    added tabular format

--- src/containers/accounts/AccountList.js
+++ src/containers/accounts/AccountList.js
@@ -1,17 +1,90 @@
 import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
-import { Link, withRouter } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import { bindActionCreators } from "redux";
+import { actions } from "../../actions/index";
+
+// mui
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+// mui icons
+import EditIcon from "@material-ui/icons/EditOutlined";
 
 class AccountList extends Component {
+  handleUserCurrentAccountUpdate = e => {};
+
+  handleAccountPageOpen = e => {
+    e.preventDefault();
+    const accountId = e.target.dataset.id;
+    this.props.history.push(`/accounts/${accountId}`);
+  };
+
   render() {
-    const { match } = this.props;
+    const { accounts } = this.props;
+
+    let accountList;
+    if (!accounts.length) {
+      accountList = (
+        <TableRow>
+          <TableCell colSpan={3}>Please create account.</TableCell>
+        </TableRow>
+      );
+    } else {
+      accountList = accounts.map(account => {
+        return (
+          <TableRow key={account.id}>
+            <TableCell>{account.company_name}</TableCell>
+            <TableCell>Update this</TableCell>
+            <TableCell>{account.active ? "Active" : "Inactive"}</TableCell>
+            <TableCell>
+              <EditIcon
+                data-id={account.id}
+                fontSize="small"
+                onClick={e => this.handleAccountPageOpen(e)}
+              />
+            </TableCell>
+          </TableRow>
+        );
+      });
+    }
+
     return (
       <Fragment>
-        <h2>Accounts List Page</h2>
+        <Paper>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Account Name</TableCell>
+                <TableCell>Subscriber</TableCell>
+                <TableCell>Status</TableCell>
+                <TableCell />
+              </TableRow>
+            </TableHead>
+            <TableBody>{accountList}</TableBody>
+          </Table>
+        </Paper>
       </Fragment>
     );
   }
 }
 
-export default withRouter(AccountList);
+const mapStateToProps = ({ accounts }) => {
+  return { accounts: accounts.accounts };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    actions: bindActionCreators(actions, dispatch)
+  };
+};
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(AccountList)
+);
